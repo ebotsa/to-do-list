895eedbae8f21fe641cb545355490d0b0ee526f3
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-08-24 20:30:04 +0200

    Create scripts.js
    added js folder & js file plus content to the to-do-list

---
+++ css/js/scripts.js
@@ -0,0 +1,33 @@
+function newItem() {
+
+    //1. Adding a new item to the list of items: 
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(document.createTextNode(inputValue));
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list = document.querySelector('#list');
+        $('#list').append(li);
+    }
+
+    //2. Crossing out an item from the list of items:
+    li.on('dblclick', function crossOut() {
+        li.addClass('strike');
+    });
+
+    //3(i). Adding the delete button "X": 
+    let crossOutButton = $('<crossOutButton> x </crossOutButton>');
+    li.append(crossOutButton);
+
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    crossOutButton.on('click', function deleteListItem() {
+        li.addClass("delete");
+    });
+
+    // 4. Reordering the items: 
+    $('#list').sortable();
+
+}
+
